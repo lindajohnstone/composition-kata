8c251df24436872ca7084b4ce021cc3dc929763c
Linda Johnstone <linda.johnstone@myob.com>
2020-10-06 17:30:37 +1100

    Inheritance tests

--- Algorithm/Inheritance/HighPassSummingAggregator.cs
+++ Algorithm/Inheritance/HighPassSummingAggregator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,8 +8,28 @@ namespace Algorithm.Inheritance
     /// Should filter out measurements with an X or Y that are less than or equal to 2
     /// You'll need to inherit and override methods from other classes in the inheritance folder
     /// </summary>
-    public class HighPassSummingAggregator
+    public class HighPassSummingAggregator : PointsAggregator
     {
 
+        public HighPassSummingAggregator(IEnumerable<Measurement> measurements) : base(measurements)
+        {  
+        }
+
+        public override Measurement Aggregate()
+        {
+            var measurements = Measurements;
+            measurements = FilterMeasurements(measurements);
+            return AggregateMeasurements(measurements);
+        }
+
+        protected override Measurement AggregateMeasurements(IEnumerable<Measurement> measurements)
+        {
+            return new Measurement { X = measurements.Sum(m => m.X), Y = measurements.Sum(m => m.Y) };
+        }
+
+        protected override IEnumerable<Measurement> FilterMeasurements(IEnumerable<Measurement> measurements)
+        {
+            return measurements.Where(m => m.X > 2 && m.Y > 2);
+        }
     }
 }
